a462c64eed7562853ca9bbe622aa84d2615e958c
mrl00 <m2l_0@outlook.com>
2025-11-16 00:24:59 -0300

    refactor: change creationdate field from string to datetime

--- internal/models/account.go
+++ internal/models/account.go
@@ -10,7 +10,7 @@ type Account struct {
 	AccountID    string    `json:"accountId" validate:"required,alphanum" db:"account_id"`
 	DistrictID   int       `json:"districtId" validate:"required,min=1" db:"district_id"`
 	Frequency    string    `json:"frequency" validate:"required" db:"frequency"`
-	CreationDate string    `json:"creationDate" validate:"required,date" db:"parseddate"`
+	CreationDate time.Time `json:"creationDate" validate:"required,date" db:"parseddate"`
 	UpdateTime   time.Time `json:"updateTime" validate:"required" db:"date"`
 }
 
@@ -20,11 +20,16 @@ func AccountMapper(line []string) (*Account, error) {
 		return nil, fmt.Errorf("[AccountMapper] :: cannot convert district_id value %s: %v", line[1], err)
 	}
 
+	creationDate, err := time.Parse("2006-01-02", line[3])
+	if err != nil {
+		return nil, fmt.Errorf("[AccountMapper] :: cannot parse creation data: %v", err)
+	}
+
 	return &Account{
 		AccountID:    line[0],
 		DistrictID:   distId,
 		Frequency:    line[2],
-		CreationDate: line[3],
+		CreationDate: creationDate,
 		UpdateTime:   time.Now(),
 	}, nil
 }
