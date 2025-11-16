ef9f4a501a155ff2e300bd8b4be0901a00ee1ede
mrl00 <m2l_0@outlook.com>
2025-11-16 00:26:00 -0300

    feat: create kafka aux functions

---
+++ internal/kafka/kafka.go
@@ -0,0 +1,117 @@
+package kafka
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"strconv"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/mrl00/stream-processing-with-apache-flink/internal/config"
+)
+
+func NewProducer(cfg config.AppConfig) (*kafka.Producer, error) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": cfg.GetBrokers(),
+		"acks":              "all",
+		"retries":           5,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create producer: %v", err)
+	}
+	return p, nil
+}
+
+func NewConsumer(cfg config.AppConfig, groupID string, topic string) (*kafka.Consumer, error) {
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":  cfg.GetBrokers(),
+		"group.id":           groupID,
+		"auto.offset.reset":  "earliest",
+		"enable.auto.commit": true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("cannot create consumer: %v", err)
+	}
+	err = consumer.SubscribeTopics([]string{topic}, nil)
+	if err != nil {
+		consumer.Close()
+		return nil, fmt.Errorf("failed to subscribe topic %s: %v", topic, err)
+	}
+
+	return consumer, nil
+}
+
+func Produce[E any](ctx context.Context, producer *kafka.Producer, e E) error {
+	message, err := json.Marshal(e)
+	if err != nil {
+		return fmt.Errorf("failed to marshal: %v", err)
+	}
+
+	topic := ctx.Value("topic").(string)
+	err = producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          message,
+	}, nil)
+	if err != nil {
+		return fmt.Errorf("failed to produce message: %v", err)
+	}
+
+	select {
+	case ev := <-producer.Events():
+		switch e := ev.(type) {
+		case *kafka.Message:
+			if e.TopicPartition.Error != nil {
+				return fmt.Errorf("delivery failed: %v", e.TopicPartition.Error)
+			}
+			fmt.Printf("Produce order: %s\n", string(message))
+
+		case kafka.Error:
+			return fmt.Errorf("producer error: %v", e)
+		}
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+
+	return nil
+}
+
+func CheckTopic(ctx context.Context, admin kafka.AdminClient, topic string) (bool, error) {
+	defer admin.Close()
+
+	metadata, err := admin.GetMetadata(&topic, false, 5000)
+	if err == nil && len(metadata.Topics) > 0 && len(metadata.Topics[topic].Partitions) > 0 {
+		slog.Log(ctx, slog.LevelDebug, "topic %s already exists", topic, nil)
+		return true, nil
+	}
+	return false, err
+}
+
+func CreateTopic(ctx context.Context, admin kafka.AdminClient, topic kafka.TopicSpecification) error {
+	exists, err := CheckTopic(ctx, admin, topic.Topic)
+	if err != nil && exists == false {
+		slog.ErrorContext(ctx, "create topic error", slog.String("err1", err.Error()))
+		return err
+	}
+
+	maxDur, err := time.ParseDuration("60s")
+	if err != nil {
+		panic("ParseDuration(60s)")
+	}
+
+	results, err := admin.CreateTopics(ctx, []kafka.TopicSpecification{topic}, kafka.SetAdminOperationTimeout(maxDur))
+	if err != nil {
+		slog.Log(ctx, slog.LevelError, "failed to create topic %s: %v", topic.Topic, err)
+		return err
+	}
+
+	for i, result := range results {
+		if result.Error.Code() != kafka.ErrNoError && result.Error.Code() != kafka.ErrTopicAlreadyExists {
+			slog.WarnContext(ctx, "%s: %s ", strconv.Itoa(i), result.Topic, result.Error.String(), nil)
+		}
+	}
+
+	slog.Debug("")
+	return nil
+}
