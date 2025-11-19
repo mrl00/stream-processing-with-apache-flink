7e615b92cf830649a30456d2bee228eaf8df2b74
mrl00 <m2l_0@outlook.com>
2025-11-19 12:18:02 -0300

    feat: implement account producer

--- cmd/account_producer/main.go
+++ cmd/account_producer/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/mrl00/stream-processing-with-apache-flink/internal/config"
 	"github.com/mrl00/stream-processing-with-apache-flink/internal/kafka"
+	"github.com/mrl00/stream-processing-with-apache-flink/internal/models"
+	"github.com/mrl00/stream-processing-with-apache-flink/internal/utils"
+)
+
+const (
+	accountTopic    = "accounts"
+	accountDataFile = "accounts.csv"
 )
 
 func main() {
@@ -18,14 +26,28 @@ func main() {
 		log.Fatalf("config error: %v", err)
 	}
 
+	if err = kafka.EnsureTopic(ctx, accountTopic, cfg); err != nil {
+		log.Fatalf("main :: ensure topic err :: %v", err)
+	}
+
 	producer, err := kafka.NewProducer(cfg)
 	if err != nil {
 		log.Fatalf("main :: produce create :: %v", err)
 	}
 	defer producer.Close()
 
-	if err = kafka.EnsureTopic(ctx, "accounts", cfg); err != nil {
-		panic(err)
+	accounts, err := utils.LoadDataFile(accountDataFile, models.AccountMapper)
+	if err != nil {
+		log.Fatalf("main :: load file :: %v", err)
 	}
 
+	accountCtx := context.WithValue(ctx, "topic", accountTopic)
+	accounts.ForEach(func(a *models.Account) {
+		if err := kafka.Produce(accountCtx, producer, a); err != nil {
+			log.Fatalf("main :: produce accounts :: %v", err)
+		}
+		time.Sleep(1 * time.Second)
+	})
+
+	select {}
 }
